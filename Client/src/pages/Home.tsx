08a1d7c75dd57e262d4c6e90c4119bf786f294b5
Guilherme de Souza <guilherme.desouzaferreira2004@gmail.com>
2025-08-20 19:14:15 -0300

    correcao de alguns pontos minimos de responsividade

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -173,7 +173,7 @@ export default function Home() {
                   <li>WhatsApp: +55 (71) 99999-9999 </li>
                   <li>Service Hours: Mon - Fri, 9 AM to 6 PM (BRT).</li>
                 </ul>
-                <ul className="text-sm space-y-2">
+                <ul className="text-sm space-y-2 text-center lg:text-left">
                   <li className="text-base font-semibold">Follow NOIR:</li>
                   <li className="flex items-center gap-2 justify-center lg:justify-start">
                     <SocialIcon
@@ -193,7 +193,7 @@ export default function Home() {
                 <p className="max-w-xs text-center lg:text-left">
                   For quick answers, check out our FAQ (
                   <Link to="/faq">
-                    <span className="text-blue-500 hover:underline">
+                    <span className="text-blue-500 relative after:content-[''] after:absolute after:bottom-[-0.02rem] after:left-[0.10rem] after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-[calc(100%-0.2rem)]">
                       Frequently Asked Questions
                     </span>
                   </Link>
